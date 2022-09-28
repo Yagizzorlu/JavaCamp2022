924596cf5f12eecd023978e6809d38ad5dc68642
Yagizzorlu <113940368+Yagizzorlu@users.noreply.github.com>
2022-09-28 19:57:14 +0300

    Add files via upload

---
+++ methods/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+    public static void main(String[] args) {
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+    }
+
+    public static void sayiBulmaca() {
+        int[] sayilar = new int[]{2, 4, 6, 8, 11};
+        int aranacak = 8;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        String mesaj = "";
+        if (varMi) {
+            mesaj = "Sayı mevcuttur: " + aranacak;
+            mesajVer(mesaj);
+        } else {
+            mesajVer("Sayı mevcut değildir: " + aranacak);
+        }
+    }
+
+
+    public static void mesajVer(String mesaj) {
+        System.out.println(mesaj);
+    }
+
+
+}
+
